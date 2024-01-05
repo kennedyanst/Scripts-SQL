979aa97d2f45ffc0b8fe7e1e306dc4526672e64a
Kennedy Anderson <keke.218@gmail.com>
2024-01-05 11:42:59 -0300

    Update Exibindo Dados a partir de multiplas tabelas (JOIN).sql

--- Oracle/Exibindo Dados a partir de multiplas tabelas (JOIN).sql
+++ Oracle/Exibindo Dados a partir de multiplas tabelas (JOIN).sql
@@ -46,3 +46,47 @@ SELECT empregado.employee_id "Id_ empregado", empregado.last_name "Sobrenome emp
 FROM employees empregado JOIN employees gerente
 ON (empregado.manager_id = gerente.employee_id)
 ORDER BY empregado.employee_id;
+
+--INNER JOIN
+
+SELECT e.employee_id, j.job_title, d.department_name, l.city, l.state_province, l.country_id
+FROM employees e
+    INNER JOIN jobs             j ON e.job_id = j.job_id
+    INNER JOIN departments      d ON d.department_id = e.department_id
+    INNER JOIN locations        l ON d.location_id = l.location_id
+ORDER BY e.employee_id;
+
+SELECT department_id, department_name, location_id, city
+FROM departments
+NATURAL INNER JOIN locations;
+
+
+--OUTER JOIN
+
+-- LEFT OUTER JOIN
+
+SELECT e.first_name, e.last_name, d.department_id, d.department_name
+FROM employees e LEFT OUTER JOIN departments d
+    ON (e.department_id = d.department_id)
+ORDER BY d.department_id;
+
+-- RIGHT OUTER JOIN
+
+SELECT d.department_id, d.department_name, e.first_name, e.last_name
+FROM employees e RIGHT OUTER JOIN departments d
+     ON (e.department_id = d.department_id)
+ORDER BY d.department_id;
+
+-- FULL OUTER JOIN
+
+SELECT d.department_id, d.department_name, e.first_name, e.last_name
+FROM employees e FULL OUTER JOIN departments d
+    ON (e.department_id = d.department_id)
+ORDER BY d.department_id;
+
+-- CROSS JOIN (Gerador de produto cartesiano)
+
+SELECT last_name, department_name
+FROM employees
+    CROSS JOIN departments;
+
