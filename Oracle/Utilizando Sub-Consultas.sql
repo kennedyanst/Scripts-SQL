e15394e4bad196997025f1853d02bba16d69e355
Kennedy Anderson <keke.218@gmail.com>
2024-01-08 12:33:00 -0300

    Update Utilizando Sub-Consultas.sql

--- Oracle/Utilizando Sub-Consultas.sql
+++ Oracle/Utilizando Sub-Consultas.sql
@@ -25,4 +25,39 @@ SELECT employee_id, first_name, last_name
 FROM employees
 WHERE last_name = (SELECT last_name
                    FROM employees
-                   WHERE last_name = 'Suzuki')
+                   WHERE last_name = 'Suzuki');
+                   
+--Sub-Consultas Multiple-row
+
+
+SELECT employee_id, first_name, last_name
+FROM employees
+WHERE salary IN 
+                (SELECT AVG(NVL(salary,0)) 
+                FROM employees 
+                GROUP BY department_id);
+                
+SELECT employee_id, last_name, job_id, salary
+FROM employees
+WHERE salary < ANY 
+                    (SELECT salary
+                    FROM employees
+                    WHERE job_id = 'IT_PROG');
+                    
+SELECT employee_id, last_name, job_id, salary
+FROM employees
+WHERE salary < ALL 
+                    (SELECT salary
+                    FROM employees
+                    WHERE job_id = 'IT_PROG');
+
+-- Cuidados com Valores nulos em uma sub-consulta com operador IN             
+SELECT emp.employee_id, emp.last_name
+FROM employees emp
+WHERE emp.employee_id IN (SELECT mgr.manager_id
+                          FROM employees mgr);
+                          
+SELECT emp.employee_id, emp.last_name
+FROM employees emp
+WHERE emp.employee_id NOT IN (SELECT mgr.manager_id
+                          FROM employees mgr);                          
