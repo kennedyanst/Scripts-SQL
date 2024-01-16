6d841a471349612cfaae42e7c1b6e044f9b16211
Kennedy Anderson <keke.218@gmail.com>
2024-01-16 12:12:01 -0300

    Update Oracle PL-SQL - Cursor Explícito.sql

--- Oracle/Oracle PL-SQL - Cursor Explícito.sql
+++ Oracle/Oracle PL-SQL - Cursor Explícito.sql
@@ -118,3 +118,25 @@ BEGIN
                              LTRIM(TO_CHAR(employees_record.salary, 'L99G999G999D99')));
     END LOOP;
 END;
+
+
+-- Cursor Explicito com SELECT FOR UPDATE
+
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+DECLARE
+    CURSOR employees_cursor (pjob_id VARCHAR2)
+    IS
+    SELECT * 
+    FROM employees
+    WHERE job_id = pjob_id
+    FOR UPDATE;
+BEGIN
+    FOR employees_record IN employees_cursor ('AD_VP')
+    LOOP
+        UPDATE employees
+        SET salary = salary * (1 + 10 / 100)
+        WHERE CURRENT OF employees_cursor;
+    END LOOP;
+    COMMIT;
+END;
