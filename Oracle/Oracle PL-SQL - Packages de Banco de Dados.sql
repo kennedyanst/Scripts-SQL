aedffb8618cdbf0f403dee3304a8a0ef6f763312
Kennedy Anderson <keke.218@gmail.com>
2024-01-20 17:39:43 -0300

    Update Oracle PL-SQL - Packages de Banco de Dados.sql

--- Oracle/Oracle PL-SQL - Packages de Banco de Dados.sql
+++ Oracle/Oracle PL-SQL - Packages de Banco de Dados.sql
@@ -112,3 +112,11 @@ gMinSalary employees.salary%TYPE
         END;
         
 END PCK_EMPREGADOS;
+
+
+-- Referenciando Componentes de uma Packege
+
+BEGIN
+    PCK_EMPREGADOS.PRC_INSERE_EMPREGADO('Bob', 'Dylan', 'BDYLAN', '515.258.4861', SYSDATE, 'IT_PROG', 20000, NULL, 103, 60)
+END;
+
