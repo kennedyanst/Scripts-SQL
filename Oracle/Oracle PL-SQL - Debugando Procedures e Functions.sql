30ef38fbf2e3a666ff1509306cee2c0b6e9f7076
Kennedy Anderson <keke.218@gmail.com>
2024-01-20 12:24:40 -0300

    Create Oracle PL-SQL - Debugando Procedures e Functions.sql

---
+++ Oracle/Oracle PL-SQL - Debugando Procedures e Functions.sql
@@ -0,0 +1,13 @@
+grant DEBUG CONNECT SESSION, DEBUG ANY PROCEDURE TO hr;
+
+BEGIN
+    DBMS_NETWORK_ACL_ADMIN.APPEND_HOST_ACE
+    (
+    host => '127.0.0.1',
+    lower_port => null,
+    upper_port => null,
+    ace => xs$ace_type(privilege_list => xs$name_list('jdwp'),
+    principal_name => 'hr',
+    principal_type => xs_acl.ptype_db)
+    );
+END;
